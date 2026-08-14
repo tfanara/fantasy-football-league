192dfe9bcacbe1e82f54e2e6ed21b72c36eb08a1
Tim <63014509+tfanara@users.noreply.github.com>
2026-08-14 10:07:01 -0400

    Initial fantasy football website

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="My Fantasy Football League",
+    page_icon="🏈"
+)
+
+st.title("🏈 My Fantasy Football League")
+
+st.write("Welcome to the greatest fantasy football league on Earth.")
+
+st.header("2026 Season")
+
+st.write("The website is under construction.")
